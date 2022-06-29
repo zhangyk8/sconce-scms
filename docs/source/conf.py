7323bd33917cf0bfbe356a3ed4efe6fae507ad9a
Yikun Zhang <30488853+zhangyk8@users.noreply.github.com>
2022-06-29 06:23:49 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SCONCE-SCMS'
+copyright = '2022, Yikun Zhang'
+author = 'Yikun Zhang'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.0.10'
 
 # -- General configuration
 
