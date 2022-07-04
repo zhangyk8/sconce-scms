e3b65d832bfb238c139b7efbeb895557ae07671c
Yikun Zhang <30488853+zhangyk8@users.noreply.github.com>
2022-07-04 02:46:04 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,11 +34,11 @@ extensions = [
     'sphinx.ext.intersphinx',
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
+# intersphinx_mapping = {
+#     'python': ('https://docs.python.org/3/', None),
+#     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+# }
+# intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
