1aaa0852f8d835a38b33b58d6b6ecbd0b657769b
Yikun Zhang <30488853+zhangyk8@users.noreply.github.com>
2022-07-04 02:40:44 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,4 +1,19 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
 
 # -- Project information
 
@@ -26,6 +41,11 @@ intersphinx_mapping = {
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 # -- Options for HTML output
 
