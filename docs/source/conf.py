75a55f6fe438def552240bc6bd765a84097360e8
Yikun Zhang <30488853+zhangyk8@users.noreply.github.com>
2022-07-04 05:13:21 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,3 +59,6 @@ master_doc = 'index'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
