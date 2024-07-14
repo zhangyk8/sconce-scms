8c721bbf3e1274b186357e5ca47380a81adfffac
Yikun Zhang <30488853+zhangyk8@users.noreply.github.com>
2024-07-13 17:23:38 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,6 +57,7 @@ exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 html_theme = 'sphinx_rtd_theme'
 master_doc = 'index'
+html_logo = "SCONCE_Sconce_Logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
